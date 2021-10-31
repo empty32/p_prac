7f4eb18fc821b36fe493198a2a5ccd99982503cb
empty32 <91768134+empty32@users.noreply.github.com>
2021-10-31 23:30:57 +0800

    Update ex43.py

--- ex43.py
+++ ex43.py
@@ -13,11 +13,14 @@ class Scene(object):
 class Engine(object):
     
     def __init__(self, scene_map):
-        self.scene_map = secne_map
+        self.scene_map = scene_map
         
-    def paly(self):
+    def play(self):
         current_scene = self.scene_map.opening_scene()
         last_scene = self.scene_map.next_scene('finished')
+        
+       # print(current_scene)
+        # print(last_cecen)
         
         while current_scene != last_scene:
             next_scene_name = current_scene.enter()
@@ -42,7 +45,7 @@ class Death(Scene):
         exit(1)
         
 
-class CentralCorrifor(Scene):
+class CentralCorridor(Scene):
     
     def enter(self):
         print(dedent("""
@@ -118,6 +121,9 @@ class LaserWeaponArmory(Scene):
         """))
         
         code = f"{randint(1, 9)}{randint(1, 9)}{randint(1, 9)}"
+        
+        print(code)
+        
         guess = input("[keypad]> ")
         guesses = 0
         
@@ -202,6 +208,7 @@ class EscapePod(Scene):
                 """))
         
         good_pod = randint(1,5)
+        print(good_pod)
         guess = input("[pod *]> ")
         
         
@@ -231,3 +238,31 @@ class Finished(Scene):
     def enter(self):
         print("You won! Good job.")
         return 'finished'
+
+    
+class Map(object):
+    
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death(),
+        'finished': Finished(),
+    }
+    
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+        
+    def next_scene(self, scene_name):
+        val = Map.scenes.get(scene_name)
+        return val
+    
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
+    
+    
+    
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
